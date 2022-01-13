e108db34fda1070e4378d06143f9ff8c343ba907
jmontanholi <jonunes9326@hotmail.com.br>
2022-01-13 20:43:39 -0300

    Add assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.scss';
 
+// ATTRIBUTIONS
+// <a href='https://br.freepik.com/fotos-vetores-gratis/computador'>Computador vetor criado por freepik - br.freepik.com</a>
+
 function App() {
   return (
     <div className="App">
