b11c9fe4461f09674dea04c3d9c684931efffcd7
jmontanholi <jonunes9326@hotmail.com.br>
2022-01-14 17:47:05 -0300

    Trying to fix margin problem home page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,13 +5,13 @@ import { GiSwordInStone } from 'react-icons/gi'
 const HomePage = () => (
   <section className={style.homepage}>
     <nav className={style.navbar}>
-      <a className={style.link} href="a">Link1</a>
-      <a className={style.link} href="a">Link2</a>
-      <a className={style.link} href="a">Link3</a>
-    </nav>
+      <a className={`${style.link} titleFont`} href="a">Home</a>
+      <a className={`${style.link} titleFont`} href="a">Login</a>
+      <a className={`${style.link} titleFont`} href="a">SignUp</a>
+      <a className={`${style.link} titleFont`} href="a">About Us</a>
+    </nav> 
     <section className={style.splashSection}>
       <div className={style.logoDiv}>
-        
         <h1 className={`${style.logo} logoFont`}><GiSwordInStone className={style.icon}/>Perspicientia</h1>
       </div>
       <div className={style.textDiv}>
