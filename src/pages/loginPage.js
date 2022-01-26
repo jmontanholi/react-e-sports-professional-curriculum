32ba070a8c69b8503058945d96f8ae07b742ae58
jmontanholi <jonunes9326@hotmail.com.br>
2022-01-26 14:11:39 -0300

    Style login page

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import dispatchCompanyLogin from '../store/api_slices/companyLogin_api';
 import dispatchUserLogin from '../store/api_slices/userLogin_api';
+import style from '../assets/stylesheets/login.module.scss';
+import { GiSwordInStone } from 'react-icons/gi'
 
 const UserLoginPage = () => {
   const dispatch = useDispatch();
@@ -25,31 +28,48 @@ const UserLoginPage = () => {
     }
   }
   return (
-    <section>
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input 
-            type="email"
-            onChange={(e) => { changeEmail(e); }}  
-          />
+    <section className={`${style.section}`}>
+      <form className={`${style.form}`}>
+        <div className={style.logoDiv}>
+          <h1 className={`${style.logo} logoFont`}><GiSwordInStone className={style.icon}/><NavLink to="/">Perspicientia</NavLink></h1>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input 
-            type="password" 
-            onChange={(e) => { changePassword(e); }}
-          />
-        </div>
-        <div>
-          <select name="user_type" id="user_type" onChange={(e) => { changeSelect(e); }}>
+        <h2 className={`${style.title} titleFont`}>Login</h2>
+        <div className={`${style.div}`}>
+          <label htmlFor="email" className={`${style.label} textFont`}>I'm a</label>
+          <select 
+            name="user_type" 
+            id="user_type" 
+            onChange={(e) => { changeSelect(e); }}
+            className={`${style.input}`}
+          >
             <option value="user">Gamer</option>
             <option value="company">Company</option>
           </select>
         </div>
+        <div className={`${style.div}`}>
+          <label htmlFor="email" className={`${style.label} textFont`}>Email</label>
+          <input
+            placeholder='example@gmail.com'
+            className={`${style.input}`}
+            type="email"
+            onChange={(e) => { changeEmail(e); }}  
+          />
+        </div>
+        <div className={`${style.div}`}>
+          <label htmlFor="password" className={`${style.label} textFont`}>Password</label>
+          <input
+            className={`${style.input}`}
+            type="password" 
+            onChange={(e) => { changePassword(e); }}
+          />
+        </div>
+        <button
+        className={`${style.submit}`} 
+        onClick={() => {dispatchRightMethod()}} 
+        >
+          Log In
+        </button>
       </form>
-      <button onClick={() => {dispatchRightMethod()}}>Log In</button>
     </section>
   )
 }
