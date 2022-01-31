9b2df219936637dc5110fbf4b5ce69681a8cf391
jmontanholi <jonunes9326@hotmail.com.br>
2022-01-31 16:52:30 -0300

    Add animations to homepage

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from "react-router-dom";
 import style from '../assets/stylesheets/homepage.module.scss';
 import { GiSwordInStone } from 'react-icons/gi';
@@ -7,32 +7,91 @@ import aboutUsImg from '../assets/images/aboutUs.jpg';
 import missionImg from '../assets/images/mission.jpg';
 import gamerImg from '../assets/images/whyBecomeGamer.jpg';
 
-const HomePage = () => (
-  <section className={style.homepage}>
-    <nav className={style.navbar}>
-      <a className={`${style.link} titleFont`} href="a">Home</a>
-      <NavLink className={`${style.link} titleFont`} to="/login">Login</NavLink>
-      <NavLink className={`${style.link} titleFont`} to="/sign_up">SignUp</NavLink>
-    </nav> 
-    <section className={style.splashSection}>
-      <div className={style.logoDiv}>
-        <h1 className={`${style.logo} logoFont`}><GiSwordInStone className={style.icon}/>Perspicientia</h1>
-      </div>
-      <div className={style.textDiv}>
-        <div className={style.rgb}/>
-        <div className={style.textInner}>
-          <p className={`${style.mainText} titleFont`}>Improve your</p>
-          <p className={`${style.changeText} titleFont`}>Chances.</p>
-          {/* <p className={`${style.changeText} titleFont`}>Game.</p>
-          <p className={`${style.changeText} titleFont`}>Perception.</p> */}
+const HomePage = () => {
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const square = entry.target;
+        if (entry.isIntersecting && square.classList.contains(style.mission)) {
+          square.classList.add(style.slideRight);
+          return;
+        }
+        
+        if (entry.isIntersecting) {
+          square.classList.add(style.slideLeft);
+          return;
+        }
+        square.classList.remove(style.slideLeft);
+        square.classList.remove(style.slideRight);
+      });
+    });
+    const aboutUs = document.querySelector(`.${style.aboutUs}`);
+    const gamerInfo = document.getElementsByClassName(style.gamerInfo);
+    const mission = document.querySelector(`.${style.mission}`)
+    observer.observe(aboutUs);
+    observer.observe(gamerInfo[0])
+    observer.observe(mission);
+  })
+  
+
+  return (
+    <section className={style.homepage}>
+      <nav className={style.navbar}>
+        <a className={`${style.link} titleFont`} href="a">Home</a>
+        <NavLink className={`${style.link} titleFont`} to="/login">Login</NavLink>
+        <NavLink className={`${style.link} titleFont`} to="/sign_up">SignUp</NavLink>
+      </nav> 
+      <section className={style.splashSection}>
+        <div className={style.logoDiv}>
+          <h1 className={`${style.logo} logoFont`}><GiSwordInStone className={style.icon}/>Perspicientia</h1>
         </div>
-      </div>
-    </section>
-    <section className={style.informationsDiv}>
-      <section className={style.aboutUs}>
-        <div>
-          <h2 className={`titleFont`}>About Us</h2>
-          <p className={`textFont`}>
+        <div className={style.textDiv}>
+          <div className={style.rgb}/>
+          <div className={style.textInner}>
+            <p className={`${style.mainText} titleFont`}>Improve your</p>
+            <p className={`${style.changeText} titleFont`}>Chances.</p>
+            {/* <p className={`${style.changeText} titleFont`}>Game.</p>
+            <p className={`${style.changeText} titleFont`}>Perception.</p> */}
+          </div>
+        </div>
+      </section>
+      <section className={style.informationsDiv}>
+        <section className={style.aboutUs}>
+          <div>
+            <h2 className={`titleFont`}>About Us</h2>
+            <p className={`textFont`}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Voluptas beatae corporis nesciunt vel pariatur sapiente ex 
+                minus vero architecto, distinctio incidunt mollitia dignissimos 
+                dolorem omnis eius soluta odio deleniti amet necessitatibus 
+                perspiciatis sed et! Sed voluptas beatae voluptatibus earum. 
+                Alias temporibus aliquid architecto, placeat ipsa rem repellat 
+                possimus ullam deserunt quisquam fuga laudantium nihil hic 
+                expedita enim sed itaque reprehenderit? Voluptates voluptatem autem quod beatae?
+            </p>
+          </div>
+          <img className={style.informationImg} src={aboutUsImg} alt="computer with controllers and user avatar images" />
+        </section>
+        <section className={style.mission}>
+          <img className={`${style.informationImg} ${style.secondImg}`} src={missionImg} alt="computer with controllers and user avatar images" />
+          <div>
+            <h2 className={`titleFont`}>Our Mission</h2>
+            <p className={`textFont`}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Voluptas beatae corporis nesciunt vel pariatur sapiente ex 
+                minus vero architecto, distinctio incidunt mollitia dignissimos 
+                dolorem omnis eius soluta odio deleniti amet necessitatibus 
+                perspiciatis sed et! Sed voluptas beatae voluptatibus earum. 
+                Alias temporibus aliquid architecto, placeat ipsa rem repellat 
+                possimus ullam deserunt quisquam fuga laudantium nihil hic 
+                expedita enim sed itaque reprehenderit? Voluptates voluptatem autem quod beatae?
+            </p>
+          </div>
+        </section>
+        <section className={style.gamerInfo}>
+          <div>
+            <h2 className={`titleFont`}>Why become a gamer</h2>
+            <p className={`textFont`}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
               Voluptas beatae corporis nesciunt vel pariatur sapiente ex 
               minus vero architecto, distinctio incidunt mollitia dignissimos 
@@ -41,66 +100,35 @@ const HomePage = () => (
               Alias temporibus aliquid architecto, placeat ipsa rem repellat 
               possimus ullam deserunt quisquam fuga laudantium nihil hic 
               expedita enim sed itaque reprehenderit? Voluptates voluptatem autem quod beatae?
-          </p>
-        </div>
-        <img className={style.informationImg} src={aboutUsImg} alt="computer with controllers and user avatar images" />
+            </p>
+          </div>
+          <img className={`${style.informationImg} gamerImg`} src={gamerImg} alt="computer with controllers and user avatar images" />
+        </section>
       </section>
-      <section className={style.mission}>
-        <img className={`${style.informationImg} ${style.secondImg}`} src={missionImg} alt="computer with controllers and user avatar images" />
-        <div>
-          <h2 className={`titleFont`}>Our Mission</h2>
-          <p className={`textFont`}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-              Voluptas beatae corporis nesciunt vel pariatur sapiente ex 
-              minus vero architecto, distinctio incidunt mollitia dignissimos 
-              dolorem omnis eius soluta odio deleniti amet necessitatibus 
-              perspiciatis sed et! Sed voluptas beatae voluptatibus earum. 
-              Alias temporibus aliquid architecto, placeat ipsa rem repellat 
-              possimus ullam deserunt quisquam fuga laudantium nihil hic 
-              expedita enim sed itaque reprehenderit? Voluptates voluptatem autem quod beatae?
-          </p>
+      <footer className={`${style.footer} titleFont`}>      
+        <div className={style.footerDiv}>
+            <div>
+              <p>Guilherme Giachellin</p>
+              <div className={style.socialDiv}>
+                <a href='https://github.com/GuilhermeGiachellin'><AiFillGithub className={style.socialIcons}/></a>
+                <a className={style.linkedin} href='https://www.linkedin.com/in/guilherme-giachellin-2599771b9/'><AiFillLinkedin className={`${style.socialIcons}`}/></a>
+                <a className={style.email} href='mailto:guilherme.giache@gmail.com'><AiOutlineMail className={`${style.socialIcons}`}/></a>
+              </div>
+            </div>
         </div>
-      </section>
-      <section className={style.gamerInfo}>
-        <div>
-          <h2 className={`titleFont`}>Why become a gamer</h2>
-          <p className={`textFont`}>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Voluptas beatae corporis nesciunt vel pariatur sapiente ex 
-            minus vero architecto, distinctio incidunt mollitia dignissimos 
-            dolorem omnis eius soluta odio deleniti amet necessitatibus 
-            perspiciatis sed et! Sed voluptas beatae voluptatibus earum. 
-            Alias temporibus aliquid architecto, placeat ipsa rem repellat 
-            possimus ullam deserunt quisquam fuga laudantium nihil hic 
-            expedita enim sed itaque reprehenderit? Voluptates voluptatem autem quod beatae?
-          </p>
+        <div className={style.footerDiv}>
+            <div>
+              <p>João Montanholi</p>
+              <div className={style.socialDiv}>
+                <a href='https://github.com/jmontanholi'><AiFillGithub className={style.socialIcons} /></a>
+                <a className={style.linkedin} href='https://www.linkedin.com/in/joaovitormontanholi/'><AiFillLinkedin className={`${style.socialIcons}`} /></a>
+                <a className={style.email} href='mailto:jonunes9326@hotmail.com.br'><AiOutlineMail className={`${style.socialIcons}`} /></a>
+              </div>
+            </div>
         </div>
-        <img className={style.informationImg} src={gamerImg} alt="computer with controllers and user avatar images" />
-      </section>
+      </footer>
     </section>
-    <footer className={`${style.footer} titleFont`}>      
-      <div className={style.footerDiv}>
-          <div>
-            <p>Guilherme Giachellin</p>
-            <div className={style.socialDiv}>
-              <a href='https://github.com/GuilhermeGiachellin'><AiFillGithub className={style.socialIcons}/></a>
-              <a className={style.linkedin} href='https://www.linkedin.com/in/guilherme-giachellin-2599771b9/'><AiFillLinkedin className={`${style.socialIcons}`}/></a>
-              <a className={style.email} href='mailto:guilherme.giache@gmail.com'><AiOutlineMail className={`${style.socialIcons}`}/></a>
-            </div>
-          </div>
-      </div>
-      <div className={style.footerDiv}>
-          <div>
-            <p>João Montanholi</p>
-            <div className={style.socialDiv}>
-              <a href='https://github.com/jmontanholi'><AiFillGithub className={style.socialIcons} /></a>
-              <a className={style.linkedin} href='https://www.linkedin.com/in/joaovitormontanholi/'><AiFillLinkedin className={`${style.socialIcons}`} /></a>
-              <a className={style.email} href='mailto:jonunes9326@hotmail.com.br'><AiOutlineMail className={`${style.socialIcons}`} /></a>
-            </div>
-          </div>
-      </div>
-    </footer>
-  </section>
-)
+  )
+}
 
 export default HomePage;
