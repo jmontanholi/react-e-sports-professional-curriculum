8dc132fc5f278a595d22cfc3313aaa387ac89ad6
jmontanholi <jonunes9326@hotmail.com.br>
2022-01-31 13:25:21 -0300

    User profile layout almost ready

--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -15,15 +15,22 @@ const UserProfile = () => {
   const [select, setSelect] = useState([
     { label: "Overwatch", id: "OW" }
   ]);
-  console.log(select);
+  const [videoNum, setVideoNum] = useState(0)
   const users = useSelector((state) => state.gamer.all_users)
   const user = users[id-1] ? users[id-1].user : null
+  const videos = user ? user.videos : null
   let profilePic;
   if (user) {
     profilePic = user.photos.filter((e) => e.photo_type === 'profile')
   }
   return (
     <section className={`${style.section}`}>
+      <nav className={`${style.nav}`}>
+        <a href="www">something</a>
+        <a href="www">something</a>
+        <a href="www">something</a>
+        <a href="www">something</a>
+      </nav>
       {user && (
         <div className={`${style.majorDiv}`}>
           <div className={`${style.infoDiv}`}>
@@ -55,29 +62,61 @@ const UserProfile = () => {
           <div className={`${style.photosDiv}`}>
             {
               user.photos.filter((e) => e.photo_type === select[0].id).map((e) => {
-                return (<img className={`${style.photo}`} key={e.id} src={e.link} alt={e.photo_type} />)
+                return (
+                <img 
+                  className={`${style.photo}`} 
+                  key={e.id} 
+                  src='https://res.cloudinary.com/hrwa243my/image/upload/v1638366288/samples/bike.jpg' 
+                  alt={e.photo_type}
+                />)
               })
             }
+            <img 
+              className={`${style.photo}`} 
+              src='https://res.cloudinary.com/hrwa243my/image/upload/v1638366288/samples/bike.jpg' 
+              alt='something'
+            />
+             <img 
+              className={`${style.photo}`} 
+              src='https://res.cloudinary.com/hrwa243my/image/upload/v1638366288/samples/bike.jpg' 
+              alt='something'
+            />
+             <img 
+              className={`${style.photo}`} 
+              src='https://res.cloudinary.com/hrwa243my/image/upload/v1638366288/samples/bike.jpg' 
+              alt='something'
+            />
+             <img 
+              className={`${style.photo}`} 
+              src='https://res.cloudinary.com/hrwa243my/image/upload/v1638366288/samples/bike.jpg' 
+              alt='something'
+            />
+             <img 
+              className={`${style.photo}`} 
+              src='https://res.cloudinary.com/hrwa243my/image/upload/v1638366288/samples/bike.jpg' 
+              alt='something'
+            />
           </div>
           <div className={`${style.videosDiv}`}>
+            <button className={`${style.leftArrow}`} type='button'>{'<'}</button>
             {
-              user.videos.filter((e) => e.video_type === select[0].id).map((e) => {
-                return (
-                <iframe
-                  key={`${e.id}`}
-                  className={`${style.video}`}
-                  width="560" 
-                  height="315" 
-                  src={e.link} 
-                  title={`video number ${e.id} type: ${e.video_type}`} 
-                  frameBorder="0" 
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                  allowFullScreen
-                >
-                </iframe>
-                )
-              })
+              user.videos.filter((e) => e.video_type === select[0].id).length === 0 && (
+                <div className={`${style.videoFrame}`}>
+                  <p className={`${style.noVideoMessage}`}>This player has no video upload for this category</p>
+                </div>
+              )
             }
+            <iframe
+              key={`${videos[videoNum].id}`}
+              className={`${style.video}`}
+              src='https://res.cloudinary.com/hrwa243my/video/upload/v1638366301/samples/sea-turtle.mp4' 
+              title={`video number ${videos[videoNum].id} type: ${videos[videoNum].video_type}`} 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+              allowFullScreen
+            >
+            </iframe>
+            <button className={`${style.rightArrow}`} type='button'>{'>'}</button>
           </div>
         </div>
       )}
